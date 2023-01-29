417fe2cfef6d6a43d07b6c693d567fae89090942
Ilya Yalymov <chezeee@gmail.com>
2023-01-29 21:20:23 +0300

    Initial structure added

---
+++ js/index.js
@@ -0,0 +1,10 @@
+import User from "./user.js";
+
+// class usage
+const sam = new User("Sam", "Blue");
+console.log(sam.getFullName());
+
+const charlie = new User("Charlie", "Doe");
+console.log(charlie.getFullName());
+
+console.log("---");
